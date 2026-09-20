1044b215707f7f3e0b6ac4706134d3c003426021
sid221722-collab <sid221722@gmail.com>
2026-09-20 23:06:15 +0300

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -2,7 +2,7 @@
 // Поддерживаются оба имени переменной для совместимости с текущим index.html.
 window.AUDIT_PWA_CONFIG = {
   CHECKER_NAME: '',
-    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqBTpXz-2VYmt3bCXA2z2nWXUe_kq526CHUxuBsm7UV70SH5EL4nz9DPyazhmfhULNuQ/exec'
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxGrRdeuETE-UnEgDboAVgqpUV0LfE0KOMcD38C6ZFCuRR84YU7uoEuMlfF_o9Hwx9CmA/exec'
 };
 window.AUDIT_APP_CONFIG = {
   GOOGLE_SCRIPT_URL: window.AUDIT_PWA_CONFIG.APPS_SCRIPT_URL
